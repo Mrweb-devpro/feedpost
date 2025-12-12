fb4d7e3ae01f9da6597a66a2100596836d867c2a
david nwokedike <miklileza@gmail.com>
2025-12-12 15:00:13 +0100

    added a local folder

--- backend/routes/api/authRoute.js
+++
@@ -1,7 +0,0 @@
-const authRoute = require("express").Router();
-
-const authController = require("../../controllers/authController");
-
-authRoute.post("/signup", authController.signup);
-
-module.exports = authRoute;

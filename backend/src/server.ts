7a9ce427b501fe534871871e94b02037be5bd9b5
david nwokedike <miklileza@gmail.com>
2025-12-12 15:37:41 +0100

    backend typescript setup

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+//--  middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//-- Routes
+// app.use("/api/auth", authRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hi this is the api base endpoint for FEEDPOST");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is listening on Port: ${PORT}`));

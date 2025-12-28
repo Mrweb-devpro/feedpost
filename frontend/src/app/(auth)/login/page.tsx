13691244c3b0491e484add9c349eb0a840c3c06c
david nwokedike <miklileza@gmail.com>
2025-12-28 14:58:58 +0100

    CLIENT:- Building login page

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,7 +1,20 @@
+import AuthImage from "@/components/AuthImage";
+import AuthInput from "@/components/inputs/AuthInput";
+import Logo from "@/components/Logo";
 import { Metadata } from "next";
 
 export const metadata: Metadata = { title: "Login" };
 
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  return (
+    <form>
+      <Logo />
+      <AuthImage />
+
+      <h1>Login</h1>
+      <AuthInput name="email" type="email" />
+      <AuthInput name="password" type="password" />
+      <button> login</button>
+    </form>
+  );
 }
